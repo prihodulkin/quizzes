2aef210e0dd618ea9d2e293abf6914aa2496d6cf
prihodulkin <45002548+prihodulkin@users.noreply.github.com>
2020-12-28 02:29:50 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   post :user_token, to: 'user_token#create'
-  put '/user/change_password', to: 'users#change_password'
   resource :user 
   resources :questions
   resources :categories
