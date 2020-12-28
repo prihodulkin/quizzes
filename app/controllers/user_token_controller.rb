d92884f88bbd28d2d558b912dfef83c3193e530c
prihodulkin <45002548+prihodulkin@users.noreply.github.com>
2020-12-29 00:07:26 +0300

    fix hope

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,4 +1,5 @@
 class UserTokenController < Knock::AuthTokenController
+  skip_before_action :verify_authenticity_token, raise: false
   private
   def auth_params
     params.require(:auth).permit(:phone, :password)
